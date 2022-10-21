39c654d976ec9b50ea525e4642b306a691f01ec5
dainiz <dainiz.almazan@gmail.com>
2022-10-21 01:21:42 -0700

    Adds Book class

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,9 @@ hello_world_bp = Blueprint("hello_world", __name__)
 def say_hello_world():
     my_beautiful_response_body = "Hello, World!"
     return my_beautiful_response_body
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
