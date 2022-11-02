a566f6d351a63bdc41b64d8c391ce58ab9f6945d
dainiz <dainiz.almazan@gmail.com>
2022-11-02 02:17:29 -0700

    adds update and delete endpoint for Book model

--- app/routes.py
+++ app/routes.py
@@ -50,3 +50,25 @@ def handle_book(book_id):
         "title": book.title,
         "description": book.description
     }
+
+@books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated", 200)
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted", 200)
