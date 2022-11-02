19dd08f22528855ea99085e1179468f4742b912c
dainiz <dainiz.almazan@gmail.com>
2022-11-02 02:08:52 -0700

    adds Book model & create method

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,23 @@
-from flask import Blueprint, jsonify
-
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
-
-books = [
-    Book(1, "Kitchen", "A novel about loss and kitchens."),
-    Book(2, "Letters to a Young Poet", "A series of letters from Rilke"),
-    Book(3, "The Catcher in the Rye", "A novel about a teenage boy in NYC")
-]
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+@books_bp.route("", methods=["POST"])
+def create_book():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
+
 @books_bp.route("", methods=["GET"])
 def handle_books():
+    books = Book.query.all()
     books_response = []
     for book in books:
         books_response.append({
@@ -24,3 +26,27 @@ def handle_books():
             "description": book.description
         })
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+
+    if not book:
+        abort(make_response({"message": f"book {book_id} not found"}, 400))
+    
+    return book
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
